d4d0f54b0b311fac64450bf20caa1fdae1912643
Nekadid <nekras10@gmail.com>
2020-04-13 21:56:48 +0200

    Update L2ex1.py
    Реализовал парсинг по всем доступным страница по кнопке "Даллее" на hh.ru. Программа работает.

--- Lesson_2/L2ex1.py
+++ Lesson_2/L2ex1.py
@@ -23,42 +23,35 @@ def salary_check(salary_in):
             else:
                 return {'top_salary': 'None', 'down_salary': 'None'}
 
+
 vacancy_name = 'инженер'
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'}
 main_link = f'https://hh.ru/search/vacancy?area=1&st=searchVacancy&text={vacancy_name}'
-html = requests.get(main_link, headers=header).text
-soup = bs(html, 'lxml')
 
-vacancy_blok = soup.find_all('div', {'class': 'vacancy-serp'})[0]
-vacancy_list = vacancy_blok.find_all('div', {'class': 'vacancy-serp-item'})
-#
 vacancies = []
-for vacancy in vacancy_list:
-    vacancy_data = {}
-    # pprint(vacancy)
-    vacancy_link = vacancy.find('a')['href']
-    vacancy_name = vacancy.find('a', {'class': 'bloko-link'}).getText()
-    vacancy_salary = vacancy.find('div', {'class': 'vacancy-serp-item__sidebar'}).getText()
+while True:
 
-    vacancy_data['name'] = vacancy_name
-    vacancy_data['link'] = vacancy_link
-    vacancy_data['down_salary'] = salary_check(vacancy_salary)['down_salary']
-    vacancy_data['top_salary'] = salary_check(vacancy_salary)['top_salary']
-    vacancies.append(vacancy_data)
-    # break
+    html = requests.get(main_link, headers=header).text
+    soup = bs(html, 'lxml')
+    vacancy_blok = soup.find_all('div', {'class': 'vacancy-serp'})[0]
+    vacancy_list = vacancy_blok.find_all('div', {'class': 'vacancy-serp-item'})
 
+    for vacancy in vacancy_list:
+        vacancy_link = vacancy.find('a')['href']
+        vacancy_name = vacancy.find('a', {'class': 'bloko-link'}).getText()
+        vacancy_salary = vacancy.find('div', {'class': 'vacancy-serp-item__sidebar'}).getText()
+
+        vacancy_data = {'name': vacancy_name,
+                        'link': vacancy_link,
+                        'down_salary': salary_check(vacancy_salary)['down_salary'],
+                        'top_salary': salary_check(vacancy_salary)['top_salary'],
+                        'website_origin': re.search(r'(?<=https://)\D{1,20}\.\D{1,3}(?=/)', main_link)[0]}
+        vacancies.append(vacancy_data)
+    try:
+        link = soup.find('a', {'class': 'bloko-button HH-Pager-Controls-Next HH-Pager-Control'})['href']
+        main_link = f'https://hh.ru{link}'
+    except TypeError:
+        break
 pprint(vacancies)
 
-
-# pprint(vacancy_salary)
-# pprint(re.sub('\xa0',' ', vacancy_salary))
-# pprint(vacancy)
-
-
-
-
-
-# vacancy_data = {}
-# vacancy_data['name'] = '200 000 руб.'
-# print(vacancy_data)
